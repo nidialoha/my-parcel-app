cbce3f8ca55f381825438af5cf801c9474f219f9
nidialoha <nidialoha@gmail.com>
2025-02-18 13:02:54 +0100

    added maps

--- src/app.js
+++ src/app.js
@@ -17,3 +17,35 @@ Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
+
+const myLocations = [
+  {
+    name: "WBS CODING SCHOOL",
+    location: [52.457131, 13.54007],
+    description: "The best coding school in the world",
+  },
+  {
+    name: "Alexanderplatz",
+    location: [52.521918, 13.413215],
+    description: "The most famous square in Berlin",
+  },
+  {
+    name: "Brandenburg Gate",
+    location: [52.516275, 13.377704],
+    description: "The most famous gate in Berlin",
+  },
+  {
+    name: "Berlin Wall",
+    location: [52.507541, 13.39032],
+    description: "The most famous wall in Berlin",
+  },
+];
+myLocations.forEach((location) => {
+  Leaflet.marker(location.location, { icon: markerIcon })
+    .bindPopup(location.description)
+    .addTo(map);
+});
+const bounds = Leaflet.latLngBounds(
+  myLocations.map((location) => location.location)
+);
+map.fitBounds(bounds);
