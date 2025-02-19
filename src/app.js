66fee2b4bda39ded6e4b51b777a3f328c32980a6
nidialoha <nidialoha@gmail.com>
2025-02-19 11:57:48 +0100

    test

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const WBS = [52.457131, 13.54007]; // WBS coordinates
 const map = Leaflet.map("map").setView(WBS, 13); // create a map object with a center and zoom level
 const markerIcon = Leaflet.icon({
   iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  iconAnchor: [10, 20],
+  iconAnchor: [10, 10],
 }); // There was an issue with the default marker icon, so we create a new one
 Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
 
